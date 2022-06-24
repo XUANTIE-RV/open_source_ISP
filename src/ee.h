b59b114bbd0ccd2626a69dd704830b24674ef6c5
Yanheng Lu <yanheng.lyh@alibaba-inc.com>
2022-06-24 19:50:27 +0800

    the first step of the long march
    Signed-off-by: Yanheng Lu <yanheng.lyh@alibaba-inc.com>

---
+++ src/ee.h
@@ -0,0 +1,15 @@
+#include "top.h"
+
+typedef struct{
+    bool eb;
+    uint8 coeff;
+}ee_register;
+
+typedef struct{
+   uint12 r;
+   uint12 g;
+   uint12 b;
+}ee_pixel_t;
+
+void edgeenhancement(top_register&, ee_register&, stream_u36&, stream_u36&);
+uint36 eeprocess(uint36[5][5], ee_register&);
